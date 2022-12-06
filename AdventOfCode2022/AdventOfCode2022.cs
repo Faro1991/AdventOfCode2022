8f94e48a9805d905d0d5c8fcb4eaf11806aacb51
Fnord <FnordGitHub@outlook.de>
2022-12-07 00:43:54 +0100

    Fix: pass accepted errors, add: XUnit to wordlist

--- AdventOfCode2022/AdventOfCode2022.cs
+++ AdventOfCode2022/AdventOfCode2022.cs
@@ -12,7 +12,11 @@ namespace AdventOfCode2022
             }
             catch (Exception e)
             {
-                if (e.Message != "Afraid it's not December yet. Please come back later.")
+                string[] allowedMessages = {
+                    "Challenge not online yet, wait for midnight EST.",
+                    "Afraid it's not December yet. Please come back later."
+                };
+                if (!allowedMessages.Contains(e.Message))
                 {
                     throw new Exception(e.Message + " (Forwarded from DailyDownloader.cs, please check it as well.)");
                 }
